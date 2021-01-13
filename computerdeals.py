de08e1df9544e58fea0e376b8ab4a5cf49da08b2
soorower <50005313+soorower@users.noreply.github.com>
2021-01-13 09:20:41 +0600

    Add files via upload

--- computerdeals.py
+++ computerdeals.py
@@ -3,17 +3,17 @@ import scrapy
 from scrapy_selenium import SeleniumRequest
 
 class ComputerdealsSpider(scrapy.Spider):
-    name = 'computerdeals'
+    name = 'computerdeals' #name of the spider
     
 
-    def start_requests(self):
+    def start_requests(self): # sending request to the url, you want to scrape
         yield SeleniumRequest (
             url = 'https://slickdeals.net/computer-deals/',
-            wait_time = 3,
-            callback = self.parse
+            wait_time = 3, # waiting 3 seconds to run next codes.It reduces possibility of getting banned
+            callback = self.parse # sending data to parse class
 
         )
-    def parse(self, response):
+    def parse(self, response): # parsing the data by selecting their xpath selectors. 
         products = response.xpath("//ul[@class='dealTiles categoryGridDeals']/li")
         for product in products:
             yield {
@@ -23,11 +23,11 @@ class ComputerdealsSpider(scrapy.Spider):
                 'price': product.xpath("normalize-space(.//div[@class='itemPrice  wide ']/text())").get()
             }
         
-        next_page = response.xpath("//a[@data-role='next-page']/@href").get()
-        if next_page:
+        next_page = response.xpath("//a[@data-role='next-page']/@href").get() #checking , if there any next page exist
+        if next_page: #  sending request to next page
             absoulute_url = f"https://slickdeals.net{next_page}"
             yield SeleniumRequest(
                 url = absoulute_url,
                 wait_time= 3,
-                callback=self.parse
+                callback=self.parse # getting response to parse class.
             )
